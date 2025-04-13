a9c563e2a48b28918612a22f05054279c694862d
Alejocaro <Alejandroc818@gmail.com>
2025-04-13 15:43:54 -0500

    configuracion github actions CI/CD

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,11 +1,7 @@
 import './commands'
-
-// Ignorar errores no controlados relacionados con Tawk
 Cypress.on('uncaught:exception', (err, runnable) => {
-  // Ignorar específicamente el error de Tawk i18next
   if (err.message.includes('t.$_Tawk.i18next is not a function')) {
-    return false; // Evita que Cypress falle la prueba
+    return false; 
   }
-  // Para otros errores, permitir que Cypress falle la prueba
   return true;
 });
